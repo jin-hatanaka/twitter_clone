72c3d474742231858a8be78ce39771b23407f3a5
jin-hatanaka <ringo.lemon15@gmail.com>
2025-04-07 16:14:00 +0900

    herokuのエラーを解消

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = %i[post get]
 end
+
+OmniAuth.config.full_host = 'https://protected-peak-36056-bd27eb82b192.herokuapp.com' if Rails.env.production?
