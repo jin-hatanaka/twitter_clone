577c77d53cdec042fd1374f438194f65f825749d
jin-hatanaka <ringo.lemon15@gmail.com>
2025-05-31 04:46:57 +0900

    いいね機能の作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,9 +8,9 @@ class ProfilesController < ApplicationController
 
   def index
     @tweets = current_user.tweets.order(created_at: 'DESC').page(params[:tweet]).per(2)
-    @like_tweets = current_user.like_tweets.order(created_at: 'DESC').page(params[:like]).per(2)
-    @retweet_tweets = current_user.retweet_tweets.order(created_at: 'DESC').page(params[:retweet]).per(2)
-    @comment_tweets = current_user.comment_tweets.order(created_at: 'DESC').page(params[:comment]).per(2)
+    @like_tweets = current_user.like_tweets.order('likes.created_at DESC').page(params[:like]).per(2)
+    @retweet_tweets = current_user.retweet_tweets.order('retweets.created_at DESC').page(params[:retweet]).per(2)
+    @comment_tweets = current_user.comment_tweets.order('comments.created_at DESC').page(params[:comment]).per(2)
   end
 
   def edit; end
